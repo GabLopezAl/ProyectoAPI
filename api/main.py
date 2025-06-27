b3e9bfdb72257ca567e860d0ab02b33e7fab29ad
GabrielHome <lopezalvaradoa749@gmail.com>
2025-06-27 04:19:03 -0600

    Post corregido

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 app = FastAPI()
 
@@ -8,6 +10,8 @@ client = MongoClient("mongodb://localhost:27017/")
 
 db = client["sistema_escolar"]
 maestros_collection = db["maestro"]
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+crypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Maestro(BaseModel):
     id: int
@@ -27,6 +31,8 @@ def obtener_maestros():
 @app.post("/maestros/")
 def insertar_maestro(maestro: Maestro):
     maestro_dict = maestro.dict()
+    maestro_dict["password"] = crypt.hash(maestro_dict["password"])
+
     maestros_collection.insert_one(maestro_dict)
     return {"mensaje": "Maestro insertado correctamente"}
 
